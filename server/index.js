0856d6c094812581bee729e66a1355b1b3decec2
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-07 14:10:28 -0600

    Update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
 
 const app = express();
+
+new ApolloServer({
+    typeDefs: ``,
+    resolvers: () => ({}),
+})
+
+
 
 app.listen(3000);
 console.log("Server is running on port 3000");
