4010981aec0448f58ac18213ba01684c817c1113
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-12 23:05:50 -0600

    Updated

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,9 +1,11 @@
 import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 export const resolvers = {
   Query: {
     hello: () => "Hello world!",
     projects: async () => await Project.find(),
+    tasks: async () => await Task.find(),
   },
   Mutation: {
     createProject: async (_, { name, description }) => {
@@ -13,6 +15,17 @@ export const resolvers = {
       });
       const savedProject = await project.save();
       return savedProject;
+    },
+    createTask: async (_, { title, projectId }) => {
+      const projectFound = await Project.findById(projectId);
+      if (!projectFound) throw new Error("Project not found");
+
+      const task = new Task({
+        title,
+        projectId,
+      });
+      const savedTask = await task.save();
+      return savedTask;
     },
   },
 };
