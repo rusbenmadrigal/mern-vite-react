8fafde340cd3ac24000fab92d096c62c9b706866
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-13 21:21:57 -0600

    Task Uppdate

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,30 +1,34 @@
-import { gql } from "apollo-server-express";
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
-export const typeDefs = gql`
-  # Queries
-  type Query {
-    hello: String
-    projects: [Project]
-    tasks: [Task]
-  }
 
-  # Mutations
-  type Mutation {
-    createProject(name: String!, description: String!): Project
-    createTask(title: String!, projectId: String!): Task
-  }
+export const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+    projects: async () => await Project.find(),
+    project: async (_, { _id }) => await Project.findById(_id),
+    task: async (_, { _id }) => await Task.findById(_id),
+    tasks: async () => await Task.find(),
+  },
+  Mutation: {
+    createProject: async (_, { name, description }) => {
+      const project = new Project({
+        name,
+        description,
+      });
+      const savedProject = await project.save();
+      return savedProject;
+    },
+    createTask: async (_, { title, projectId }) => {
+      const projectFound = await Project.findById(projectId);
+      if (!projectFound) throw new Error("Project not found");
 
-  # Objects
-  type Project {
-    _id: ID!
-    name: String!
-    description: String!
-    tasks: [Task]!
-  }
-
-  type Task {
-    _id: ID!
-    title: String!
-    project: Project!
-  }
-`;
+      const task = new Task({
+        title,
+        projectId,
+      });
+      const savedTask = await task.save();
+      return savedTask;
+    },
+  },
+};
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,32 +1,32 @@
-import { gql } from 'graphql-tag'
-
+import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-    type Query {
-        hello: String
-        projects: [Project]
-        tasks: [Task]
-    }
+  type Query {
+    hello: String
+    projects: [Project]
+    project(_id: ID!): Project
+    task(_id: ID!): Task
+    tasks: [Task]
+  }
 
-    type Mutation {
-        createProject(name: String, description: String): Project
-        createTask(title: String, projectId: ID): Task
-    }
+  type Mutation {
+    createProject(name: String, description: String): Project
+    createTask(title: String, projectId: ID): Task
+  }
 
-    type Project {
-        _id: ID
-        name: String
-        description: String
-        createdAt: String
-        updatedAt: String
-    }
+  type Project {
+    _id: ID
+    name: String
+    description: String
+    createdAt: String
+    updatedAt: String
+  }
 
-    type Task { 
-        _id: ID
-        title: String
-        projectId: ID
-        createdAt: String
-        updatedAt: String
-    }
-
-`
+  type Task {
+    _id: ID
+    title: String
+    projectId: ID
+    createdAt: String
+    updatedAt: String
+  }
+`;
