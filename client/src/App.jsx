5b5049fa7a74efd062d15e4912f297a499d45565
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-17 11:00:28 -0600

    Client Side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Projects } from "./pages/Projects";
+import { ProjecDetails } from "./pages/ProjectDetails"
+
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div>Hello World</div>;
+  <ApolloProvider client={client}>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/projects" element={<Projects />} />
+  <Route path="/projects/:projectId" element={<ProjecDetails />} />
+  </Routes>
+  </BrowserRouter>
+  </ApolloProvider>;
 }
 
 export default App;
