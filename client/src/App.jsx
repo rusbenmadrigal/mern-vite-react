35793169ee26e6ecbda4fd2e4e2334c1a9446b9a
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-17 21:53:30 -0600

    Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Projects } from "./pages/Projects";
-import { ProjecDetails } from "./pages/ProjectDetails"
-
-
+import { ProjecDetails } from "./pages/ProjectDetails";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -11,14 +9,17 @@ const client = new ApolloClient({
 });
 
 function App() {
-  <ApolloProvider client={client}>
-  <BrowserRouter>
-  <Routes>
-  <Route path="/projects" element={<Projects />} />
-  <Route path="/projects/:projectId" element={<ProjecDetails />} />
-  </Routes>
-  </BrowserRouter>
-  </ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/projects" />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:projectId" element={<ProjecDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
