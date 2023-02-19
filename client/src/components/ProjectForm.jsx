ae484de3092f2b9e456f4815f2c1c1721bd4d056
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-19 16:31:14 -0600

    Tasks update

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_PROJECT, GET_PROJECTS } from "../graphql/projects";
+
 export function ProjectForm() {
+  const [Project, setProject] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [createProject, { loading, error }] = useMutation(CREATE_PROJECT, {
+    refetchQueries: [
+      {
+        query: GET_PROJECTS,
+      },
+      "GetProjects",
+    ],
+  });
+
+  const handleChange = (e) => {
+    setProject({
+      ...Project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProject({
+      variables: {
+        name: Project.name,
+        description: Project.description,
+      },
+    });
+  };
+
   return (
-    <div>
-      <h1>ProjectForm</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {error && <p>{error.message}</p>}
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Write a title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        rows="3"
+        placeholder="Write a description"
+        onChange={handleChange}
+      ></textarea>
+      <button disabled={!Project.name || !Project.description || loading}>
+        Create
+      </button>
+    </form>
   );
 }
--- client/src/graphql/projects.js
+++ client/src/graphql/projects.js
@@ -9,3 +9,13 @@ export const GET_PROJECTS = gql`
     }
   }
 `;
+
+export const CREATE_PROJECT = gql`
+  mutation ($name: String, $description: String) {
+    createProject(name: $name, description: $description) {
+      _id
+      name
+      description
+    }
+  }
+`;
