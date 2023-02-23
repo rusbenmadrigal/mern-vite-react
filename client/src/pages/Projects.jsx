de61562020d59eebac8932e47c33c378fc341504
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-22 19:54:40 -0600

    Update

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,11 +4,13 @@ import { ProjectList } from "../components/ProjectList";
 export function Projects() {
   return (
     <div className="bg-zinc-900 rounded-lg shadow-lg shadow-black p-8 h-3/5 w-3/5">
-     <h1 className="text-2x1 font-bold py-2 mb-4">Apollo reminder App by Russ Madrigal</h1>
-     <div className="flex justify-center gap-x-1">
-     <ProjectForm />
-      <ProjectList />
-     </div>
+      <h1 className="flex flex-col justify-center items-center text-2xl font-bold py-2 mb-4">
+        List of reminders in ReactJS by Russ
+      </h1>
+      <div className="flex justify-center gap-x-1">
+        <ProjectForm />
+        <ProjectList />
+      </div>
     </div>
   );
 }
